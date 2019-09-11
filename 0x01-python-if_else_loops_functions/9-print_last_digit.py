c4434bae7a5b7f7fbc4f386b37a4b0b3095696e6
zelda096 <miguelangel697@gmail.com>
2019-09-10 21:20:24 -0500

    add task9

---
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def print_last_digit(number):
+    if number >= 0:
+        lastdigit = number % 10
+    else:
+        lastdigit = number % -10
+        lastdigit *= -1
+
+    print("{:d}".format(lastdigit), end='')
+    return (lastdigit)
