6f7055042d89d19ac8eb6cd385863c76e68f7937
zelda096 <miguelangel697@gmail.com>
2020-02-04 14:19:05 -0500

    [Feature]: this print x times c is fun

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+// this prints x times "c is fun"
+
+if (isNaN(process.argv[2])) {
+  console.log('Missing number of occurrences');
+} else {
+  for (let i = 0; i < process.argv[2]; i++) {
+    console.log('C is fun');
+  }
+}
