269edfc2356426b471488048e399f4e4bcc0f84b
zelda096 <miguelangel697@gmail.com>
2020-02-04 13:37:24 -0500

    [Feature]: this parse the second argv in the commands line

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,10 +1,7 @@
 #!/usr/bin/node
 // this parsed or not the args in the commands line
-if (isNaN(process.argv[2]))
-{
-    console.log('Not a number');
-}
-else
-{
-    console.log('My number: ' + parseInt(process.argv[2]));
+if (isNaN(process.argv[2])) {
+  console.log('Not a number');
+} else {
+  console.log('My number: ' + parseInt(process.argv[2]));
 }
