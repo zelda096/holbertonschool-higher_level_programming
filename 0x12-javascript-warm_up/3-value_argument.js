95636703304d1a0a56e7c01722907c7fb8ca7481
zelda096 <miguelangel697@gmail.com>
2020-02-04 12:57:32 -0500

    [Feature]:this print the argv that pass in the command line

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,10 +1,7 @@
 #!/usr/bin/node
 // this print the argv pass in the command line
-if (process.argv[2])
-{
-    console.log(process.argv[2]);
-}
-else
-{
-    console.log('No Argument');
+if (process.argv[2]) {
+  console.log(process.argv[2]);
+} else {
+  console.log('No argument');
 }
