1dfac653c9c370f4762be5efd043062c2d3325da
zelda096 <miguelangel697@gmail.com>
2020-02-04 13:59:16 -0500

    [Feature]: this print three lines with a loop

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+// this prints the array of strings that pass for the const
+
+const array = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+
+for (let i = 0; i < 3; i++) {
+  console.log(array[i]);
+}
