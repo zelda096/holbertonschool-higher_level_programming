13c7464bbfa7396eadb4cfc9b3bfd8c05ee749a0
zelda096 <miguelangel697@gmail.com>
2020-02-04 16:48:00 -0500

    [Feature]:this script add a new function and increment the integer value

---
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+
+const myObject = {
+  type: 'object',
+  value: 12
+};
+console.log(myObject);
+
+myObject.incr = function () {
+  this.value += 1;
+  return this;
+};
+
+myObject.incr();
+console.log(myObject);
+myObject.incr();
+console.log(myObject);
+myObject.incr();
+console.log(myObject);
